928169a57c329525552dd92fc18ab5493ba88a47
zlle6 <100070608+zlle6@users.noreply.github.com>
2024-10-02 19:47:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+import tkinter as tk
+import webbrowser
+
+import yes
+
+#root = tk.Tk()
+#root.geometry("250x250")
+#root.title("first")
+
+#entery = tk.Entry(root, width=15, font= ("Arial", 14))
+#entery.pack(pady=120)
+
+
+print('welcome to quiz game')
+
+userinput= input("press 1 to continue: ")
+
+
+quest = 'how many ppl are there in world?'
+
+if userinput == '1':
+    print(quest)
+
+else:
+    print("Exiting the game. Goodbye!")
+    exit()
+pass
+
+
+
+
+options=[    8    ,    6    ,    7    ,    4    ]
+print(options)
+answer = input ("type down: ")
+1
+if answer =='8':
+    print("correct")
+elif answer =='6':
+     print("wrong")
+elif answer =='4':
+     print("wrong")
+elif answer == '7':
+     print("wrong")
+
+
+else: print("shame on you, Exiting the game. Goodbye!")
+exit();
+pass
+
+
+
+quest1 = 'will israel attack iraq?'
+
+
+user= input("if you want to contniue press 2: ")
+if user == '2':
+    print(quest1)
+
+else:
+    print("Exiting the game. Goodbye!")
+    exit()
+pass
+
+
+quest1 = ('will israel attack iraq?')
+
+optionss=[    'Yes'    ,    'No'   ,    'Maybe'   ,    'Never'   ]
+print(optionss)
+
+
+answer1 = input ("type down: ")
+if answer1 =='yes':
+    print("correct")
+elif answer1 =='no':
+     print("wrong")
+elif answer1 =='maybe':
+     print("correct")
+elif answer1 == 'never':
+     print("wrong")
+else: print("shame on you")
+
+pass
+
+quest2= 'does kurdistan will earse?'
+user2= input("if you want to contniue press 3: ")
+if user2 == '3':
+    print(quest2)
+
+
+
+
+
+
+#root.mainloop()
